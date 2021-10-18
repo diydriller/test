b4fe464759333c70e5f7f5d42c0a238a600034fa
diydriller <dhrhd080@naver.com>
2021-10-18 11:05:58 +0900

    INIT project

---
+++ src/main/java/com/jenkins/hello/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.jenkins.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    String index(){
+        return "hello";
+    }
+}
